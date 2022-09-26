72347089a523f567e5ef8c2f5b4ef2480a67e6b7
junengt <95681705+junengt@users.noreply.github.com>
2022-09-26 22:03:36 +0900

    PhoneAuthRepository 수정
    - 휴대전화 번호 + 통신사 + 인증 유형 조회 메소드 추가

--- src/main/java/com/example/joinandlogin/repository/PhoneAuthRepository.java
+++ src/main/java/com/example/joinandlogin/repository/PhoneAuthRepository.java
@@ -1,7 +1,14 @@
 package com.example.joinandlogin.repository;
 
 import com.example.joinandlogin.domain.PhoneAuth;
+import com.example.joinandlogin.util.code.AuthTypeCode;
+import com.example.joinandlogin.util.code.TelecomCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PhoneAuthRepository extends JpaRepository<PhoneAuth, Long> {
+
+    Optional<PhoneAuth> findByEncryptedPhoneNumberAndTelecomCodeAndAuthTypeCodeOrderByUpdatedDateDesc(String encryptedPhoneNumber,
+                                                                                TelecomCode telecomCode, AuthTypeCode authTypeCode);
 }
