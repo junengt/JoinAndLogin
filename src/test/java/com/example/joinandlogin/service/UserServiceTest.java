fab1671243c91eef7c3888eb7cef901aaaa8cc91
junengt <95681705+junengt@users.noreply.github.com>
2022-09-27 17:34:47 +0900

    UserServiceTest 수정
    - UserServiceTest 테스트 로직 수정

--- src/test/java/com/example/joinandlogin/service/UserServiceTest.java
+++ src/test/java/com/example/joinandlogin/service/UserServiceTest.java
@@ -4,7 +4,6 @@ import com.example.joinandlogin.domain.User;
 import com.example.joinandlogin.dto.JoinUserReqDto;
 import com.example.joinandlogin.dto.LoginUserReqDto;
 import com.example.joinandlogin.repository.UserRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,7 +15,6 @@ import javax.persistence.Column;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
@@ -29,37 +27,24 @@ class UserServiceTest {
     private UserService userService;
 
     @Test
-    @Column
     public void joinSuccess() throws Exception {
-        JoinUserReqDto joinUserReqDto = new JoinUserReqDto();
-        joinUserReqDto.setLoginId("test001");
-        joinUserReqDto.setEmail("test@test.com");
-        joinUserReqDto.setName("테스트");
-        joinUserReqDto.setNickname("테스트닉네임");
-        joinUserReqDto.setPassword("testPassword");
-        joinUserReqDto.setPhoneNumber("01012345678");
+        JoinUserReqDto joinUserReqDto = new JoinUserReqDto("test001", "test@test.com", "테스트닉네임",
+                "testPassword", "테스트", "01012345678");
 
         Long joinUser = userService.joinUser(joinUserReqDto);
         Optional<User> userOptional = userRepository.findById(joinUser);
         User user = userOptional.get();
 
-        assertThat(joinUser).isEqualTo(user.getId());
+        assertThat(joinUser).isEqualTo(user.getUserId());
     }
 
     @Test
     @Commit
     public void loginSuccess() throws Exception {
-        JoinUserReqDto joinUserReqDto = new JoinUserReqDto();
-        joinUserReqDto.setLoginId("test001");
-        joinUserReqDto.setEmail("test@test.com");
-        joinUserReqDto.setName("테스트");
-        joinUserReqDto.setNickname("테스트닉네임");
-        joinUserReqDto.setPassword("testPassword");
-        joinUserReqDto.setPhoneNumber("01012345678");
+        JoinUserReqDto joinUserReqDto = new JoinUserReqDto("test001", "test@test.com", "테스트닉네임",
+                "testPassword", "테스트", "01012345678");
 
-        LoginUserReqDto loginUserReqDto = new LoginUserReqDto();
-        loginUserReqDto.setLoginId("test001");
-        loginUserReqDto.setPassword("testPassword");
+        LoginUserReqDto loginUserReqDto = new LoginUserReqDto("test001", "testPassword");
 
         Long joinUserId = userService.joinUser(joinUserReqDto);
         Long loginUserId = userService.login(loginUserReqDto);
@@ -69,13 +54,13 @@ class UserServiceTest {
         User joinUser = userRepository.findById(joinUserId).get();
         User loginUser = userRepository.findById(loginUserId).get();
 
-        System.out.println("joinUser.getEncodedEmail() = " + joinUser.getEncodedEmail());
-        System.out.println("joinUser.getEncodedPassword() = " + joinUser.getEncodedPassword());
-        System.out.println("joinUser.getEncodedPhoneNumber() = " + joinUser.getEncodedPhoneNumber());
+        System.out.println("joinUser.getEncryptedEmail() = " + joinUser.getEncryptedEmail());
+        System.out.println("joinUser.getEncryptedPassword() = " + joinUser.getEncryptedPassword());
+        System.out.println("joinUser.getEncryptedPhoneNumber() = " + joinUser.getEncryptedPhoneNumber());
 
-        System.out.println("loginUser.getEncodedEmail() = " + loginUser.getEncodedEmail());
-        System.out.println("loginUser.getEncodedPassword() = " + loginUser.getEncodedPassword());
-        System.out.println("loginUser.getEncodedPhoneNumber() = " + loginUser.getEncodedPhoneNumber());
+        System.out.println("loginUser.getEncryptedEmail() = " + loginUser.getEncryptedEmail());
+        System.out.println("loginUser.getEncryptedPassword() = " + loginUser.getEncryptedPassword());
+        System.out.println("loginUser.getEncryptedPhoneNumber() = " + loginUser.getEncryptedPhoneNumber());
     }
 
 }
